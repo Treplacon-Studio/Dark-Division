359a5943577dc19da189ce3a9a921bf4633d9b0d
austynwhaley <austyn_whaley@yahoo.com>
2024-05-29 12:49:55 -0400

    inital commit for map vote

---
+++ Assets/MapVote.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapVote : MonoBehaviour
+{
+    public Image map1;
+    public Image map2;
+    
+    // vote count
+    private int map1Votes = 0;
+    private int map2Votes = 0;
+
+
+    // function to tally votes
+
+
+
+}
