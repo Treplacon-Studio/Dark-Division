89a78450364104179f185c3362b8df5d3c092dd5
Skylar Masson <Smasson1998@gmail.com>
2024-06-01 00:02:04 -0500

    working on RPC bug still.

--- Assets/6v6Shooter/Scripts/Networking/LobbyManager.cs
+++ Assets/6v6Shooter/Scripts/Networking/LobbyManager.cs
@@ -16,14 +16,27 @@ public class LobbyManager : MonoBehaviourPunCallbacks, IPunObservable
     [SerializeField] TMP_Text gameModeNameText;
     [SerializeField] TMP_Text countdownDisplay;
     [SerializeField] TMP_Text playerCountDisplay;
+    [SerializeField] TMP_Text votesForMapOneDisplay;
+    [SerializeField] TMP_Text votesForMapTwoDisplay;
+    [SerializeField] TMP_Text votesForRandomMapDisplay;
 
     private PhotonView photonView;
     private int currentTime;
 
+    private int votesForMapOne;
+    private int votesForMapTwo;
+    private int votesForRandomMap;
+
     void Awake() {
         if (instance == null) instance = this;
         photonView = GetComponent<PhotonView>();
-        Debug.Log($"PhotonView ID: {photonView.ViewID}");
+
+        if (photonView == null) {
+            Debug.LogError("PhotonView component missing. Adding PhotonView component.");
+            photonView = gameObject.AddComponent<PhotonView>();
+        }
+        
+        Debug.Log($"PhotonView ID in Awake: {photonView.ViewID}");
     }
 
     void Start() {
@@ -55,6 +68,21 @@ public class LobbyManager : MonoBehaviourPunCallbacks, IPunObservable
         }
     }
 
+    public void OnVoteForMapSelected(int mapNum) {
+        switch (mapNum) {
+            case 1:
+                votesForMapOne++;
+                break;
+            case 2:
+                votesForMapTwo++;
+                break;
+            case 3:
+                votesForRandomMap++;
+                break;
+        }
+        photonView.RPC("UpdateMapVotes", RpcTarget.AllBuffered);
+    }
+
     [PunRPC]
     public void StartTimer(int startTime) {
         currentTime = startTime;
@@ -77,7 +105,19 @@ public class LobbyManager : MonoBehaviourPunCallbacks, IPunObservable
     }
 
     [PunRPC]
+    private void UpdateMapVotes() {
+        if (photonView.ViewID != 0) {
+            votesForMapOneDisplay.text = $"VOTES: {votesForMapOne}";
+            votesForMapTwoDisplay.text = $"VOTES: {votesForMapTwo}";
+            votesForRandomMapDisplay.text = $"VOTES: {votesForRandomMap}";
+        } else {
+            Debug.LogError("PhotonView ID is 0, cannot update map votes.");
+        }
+    }
+
+    [PunRPC]
     private void UpdatePlayerCount() {
+        Debug.Log($"UpdatePlayerCount called, PhotonView ID: {photonView.ViewID}, PlayerCount: {PhotonNetwork.CurrentRoom.PlayerCount}");
         if (photonView.ViewID != 0) {
             playerCountDisplay.text = $"{PhotonNetwork.CurrentRoom.PlayerCount}/12 PLAYERS";
         } else {
@@ -102,8 +142,15 @@ public class LobbyManager : MonoBehaviourPunCallbacks, IPunObservable
 
     public override void OnJoinedRoom() {
         TeamManager.AssignTeam(PhotonNetwork.LocalPlayer);
-        photonView.RPC("UpdatePlayerCount", RpcTarget.All);
+        Debug.Log($"OnJoinedRoom called, PhotonView ID: {photonView.ViewID}");
+        StartCoroutine(UpdatePlayerCountAfterDelay());
         ListPlayers();
+    }
+
+    private IEnumerator UpdatePlayerCountAfterDelay() {
+        yield return new WaitUntil(() => photonView.ViewID != 0);
+        Debug.Log($"PhotonView ID after delay: {photonView.ViewID}");
+        photonView.RPC("UpdatePlayerCount", RpcTarget.AllBuffered);
     }
 
     public override void OnLeftRoom() {
@@ -116,12 +163,14 @@ public class LobbyManager : MonoBehaviourPunCallbacks, IPunObservable
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer) {
-        photonView.RPC("UpdatePlayerCount", RpcTarget.All);
+        Debug.Log($"OnPlayerEnteredRoom called for {newPlayer.NickName}, PlayerCount: {PhotonNetwork.CurrentRoom.PlayerCount}");
+        photonView.RPC("UpdatePlayerCount", RpcTarget.AllBuffered);
         ListPlayers();
     }
 
     public override void OnPlayerLeftRoom(Player otherPlayer) {
-        photonView.RPC("UpdatePlayerCount", RpcTarget.All);
+        Debug.Log($"OnPlayerLeftRoom called for {otherPlayer.NickName}, PlayerCount: {PhotonNetwork.CurrentRoom.PlayerCount}");
+        photonView.RPC("UpdatePlayerCount", RpcTarget.AllBuffered);
         ListPlayers();
     }
 
@@ -141,10 +190,14 @@ public class LobbyManager : MonoBehaviourPunCallbacks, IPunObservable
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
         if (stream.IsWriting) {
             stream.SendNext(currentTime);
-            stream.SendNext(playerCountDisplay);
+            stream.SendNext(votesForMapOne);
+            stream.SendNext(votesForMapTwo);
+            stream.SendNext(votesForRandomMap);
         } else {
             currentTime = (int)stream.ReceiveNext();
-            playerCountDisplay = (TMP_Text)stream.ReceiveNext();
+            votesForMapOne = (int)stream.ReceiveNext();
+            votesForMapTwo = (int)stream.ReceiveNext();
+            votesForRandomMap = (int)stream.ReceiveNext();
         }
     }
 
