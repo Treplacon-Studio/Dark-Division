5651820dc9d4cbe20655ffd3f2f13ccc9e52a41e
austynwhaley <austyn_whaley@yahoo.com>
2024-06-04 14:12:03 -0400

    update lobby voting

--- Assets/6v6Shooter/Scripts/Networking/LobbyManager.cs
+++ Assets/6v6Shooter/Scripts/Networking/LobbyManager.cs
@@ -1,8 +1,9 @@
-using Photon.Pun;
 using UnityEngine;
 using Photon.Realtime;
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
+using Photon.Pun;
 
 public class LobbyManager : MonoBehaviourPunCallbacks, IPunObservable
 {
@@ -26,34 +27,44 @@ public class LobbyManager : MonoBehaviourPunCallbacks, IPunObservable
     private int votesForMapTwo;
     private int votesForRandomMap;
 
-    void Awake() {
+    private Dictionary<int, int> playerVotes = new Dictionary<int, int>();
+
+    void Awake()
+    {
         if (instance == null) instance = this;
         photonView = GetComponent<PhotonView>();
 
-        if (photonView == null) {
+        if (photonView == null)
+        {
             Debug.LogError("PhotonView component missing. Adding PhotonView component.");
             photonView = gameObject.AddComponent<PhotonView>();
         }
-        
+
         Debug.Log($"PhotonView ID in Awake: {photonView.ViewID}");
     }
 
-    void Start() {
+    void Start()
+    {
         ConnectToPhotonServer();
     }
 
-    private void ClearPlayerListings() {
-        for (int i = teamAPlayersContainer.childCount - 1; i >= 0; i--) {
+    private void ClearPlayerListings()
+    {
+        for (int i = teamAPlayersContainer.childCount - 1; i >= 0; i--)
+        {
             Destroy(teamAPlayersContainer.GetChild(i).gameObject);
         }
-        for (int i = teamBPlayersContainer.childCount - 1; i >= 0; i--) {
+        for (int i = teamBPlayersContainer.childCount - 1; i >= 0; i--)
+        {
             Destroy(teamBPlayersContainer.GetChild(i).gameObject);
         }
     }
 
-    public void ListPlayers() {
+    public void ListPlayers()
+    {
         ClearPlayerListings();
-        foreach (Player player in PhotonNetwork.PlayerList) {
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
             Team? team = TeamManager.GetTeam(player);
             GameObject tempListing;
             if (team == Team.Blue)
@@ -68,31 +79,59 @@ public class LobbyManager : MonoBehaviourPunCallbacks, IPunObservable
         }
     }
 
-    public void OnVoteForMapSelected(int mapNum) {
-        switch (mapNum) {
+    public void OnVoteForMapSelected(int mapNum)
+    {
+        int playerID = PhotonNetwork.LocalPlayer.ActorNumber;
+
+        if (playerVotes.TryGetValue(playerID, out int previousVote))
+        {
+            // Subtract the previous vote
+            switch (previousVote)
+            {
+                case 1:
+                    votesForMapOne--;
+                    break;
+                case 2:
+                    votesForMapTwo--;
+                    break;
+                case 3:
+                    votesForRandomMap--;
+                    break;
+            }
+        }
+
+        // Add the new vote
+        switch (mapNum)
+        {
             case 1:
                 votesForMapOne++;
-                photonView.RPC("UpdateMapVotes", RpcTarget.AllBuffered);
                 break;
             case 2:
                 votesForMapTwo++;
-                photonView.RPC("UpdateMapVotes", RpcTarget.AllBuffered);
                 break;
             case 3:
                 votesForRandomMap++;
-                photonView.RPC("UpdateMapVotes", RpcTarget.AllBuffered);
                 break;
         }
+
+        // Update the player's vote
+        playerVotes[playerID] = mapNum;
+
+        // Update the votes display
+        photonView.RPC("UpdateMapVotes", RpcTarget.AllBuffered);
     }
 
     [PunRPC]
-    public void StartTimer(int startTime) {
+    public void StartTimer(int startTime)
+    {
         currentTime = startTime;
         StartCoroutine(CountdownRoutine());
     }
 
-    private IEnumerator CountdownRoutine() {
-        while (currentTime > 0) {
+    private IEnumerator CountdownRoutine()
+    {
+        while (currentTime > 0)
+        {
             photonView.RPC("UpdateTimerDisplay", RpcTarget.All, currentTime);
             yield return new WaitForSeconds(1);
             currentTime--;
@@ -102,100 +141,127 @@ public class LobbyManager : MonoBehaviourPunCallbacks, IPunObservable
     }
 
     [PunRPC]
-    private void UpdateTimerDisplay(int time) {
+    private void UpdateTimerDisplay(int time)
+    {
         countdownDisplay.text = $"TIME BEFORE MATCH STARTS: {time.ToString()}";
     }
 
     [PunRPC]
-    private void UpdateMapVotes() {
-        if (photonView.ViewID != 0) {
+    private void UpdateMapVotes()
+    {
+        if (photonView.ViewID != 0)
+        {
             votesForMapOneDisplay.text = $"VOTES: {votesForMapOne}";
             votesForMapTwoDisplay.text = $"VOTES: {votesForMapTwo}";
             votesForRandomMapDisplay.text = $"VOTES: {votesForRandomMap}";
-        } else {
+        }
+        else
+        {
             Debug.LogError("PhotonView ID is 0, cannot update map votes.");
         }
     }
 
     [PunRPC]
-    private void UpdatePlayerCount() {
+    private void UpdatePlayerCount()
+    {
         Debug.Log($"UpdatePlayerCount called, PhotonView ID: {photonView.ViewID}, PlayerCount: {PhotonNetwork.CurrentRoom.PlayerCount}");
-        if (photonView.ViewID != 0) {
+        if (photonView.ViewID != 0)
+        {
             playerCountDisplay.text = $"{PhotonNetwork.CurrentRoom.PlayerCount}/12 PLAYERS";
-        } else {
+        }
+        else
+        {
             Debug.LogError("PhotonView ID is 0, cannot update player count.");
         }
     }
 
     [PunRPC]
-    public void LoadMap() {
+    public void LoadMap()
+    {
         GameManager.instance.StartLoadingBar("S04_PublicMatch", true);
     }
 
-    public void ConnectToPhotonServer() {
+    public void ConnectToPhotonServer()
+    {
         if (!PhotonNetwork.IsConnected)
             PhotonNetwork.ConnectUsingSettings();
     }
 
-    public override void OnConnectedToMaster() {
+    public override void OnConnectedToMaster()
+    {
         Debug.Log(PhotonNetwork.NickName + " connected to Photon Server");
         PhotonNetwork.JoinRandomOrCreateRoom();
     }
 
-    public override void OnJoinedRoom() {
+    public override void OnJoinedRoom()
+    {
         TeamManager.AssignTeam(PhotonNetwork.LocalPlayer);
         Debug.Log($"OnJoinedRoom called, PhotonView ID: {photonView.ViewID}");
         StartCoroutine(UpdatePlayerCountAfterDelay());
         ListPlayers();
     }
 
-    private IEnumerator UpdatePlayerCountAfterDelay() {
+    private IEnumerator UpdatePlayerCountAfterDelay()
+    {
         yield return new WaitUntil(() => photonView.ViewID != 0);
         Debug.Log($"PhotonView ID after delay: {photonView.ViewID}");
         photonView.RPC("UpdatePlayerCount", RpcTarget.AllBuffered);
     }
 
-    public override void OnLeftRoom() {
+    public override void OnLeftRoom()
+    {
         GameManager.instance.StartLoadingBar("S00_MainMenu", false);
     }
 
-    public void LeaveRoom() {
+    public void LeaveRoom()
+    {
         TeamManager.LeaveTeam(PhotonNetwork.LocalPlayer);
         PhotonNetwork.LeaveRoom();
     }
 
-    public override void OnPlayerEnteredRoom(Player newPlayer) {
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
         Debug.Log($"OnPlayerEnteredRoom called for {newPlayer.NickName}, PlayerCount: {PhotonNetwork.CurrentRoom.PlayerCount}");
         photonView.RPC("UpdatePlayerCount", RpcTarget.AllBuffered);
         ListPlayers();
     }
 
-    public override void OnPlayerLeftRoom(Player otherPlayer) {
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
         Debug.Log($"OnPlayerLeftRoom called for {otherPlayer.NickName}, PlayerCount: {PhotonNetwork.CurrentRoom.PlayerCount}");
         photonView.RPC("UpdatePlayerCount", RpcTarget.AllBuffered);
         ListPlayers();
     }
 
-    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps) {
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
         base.OnPlayerPropertiesUpdate(targetPlayer, changedProps);
-        if (changedProps.ContainsKey("team")) {
+        if (changedProps.ContainsKey("team"))
+        {
             Team? team = TeamManager.GetTeam(targetPlayer);
-            if (team.HasValue) {
+            if (team.HasValue)
+            {
                 Debug.Log($"{targetPlayer.NickName} assigned to {team.Value} team.");
-            } else {
+            }
+            else
+            {
                 Debug.Log($"{targetPlayer.NickName} has not been assigned to any team.");
             }
             ListPlayers();
         }
     }
 
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
-        if (stream.IsWriting) {
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
             stream.SendNext(currentTime);
             stream.SendNext(votesForMapOne);
             stream.SendNext(votesForMapTwo);
             stream.SendNext(votesForRandomMap);
-        } else {
+        }
+        else
+        {
             currentTime = (int)stream.ReceiveNext();
             votesForMapOne = (int)stream.ReceiveNext();
             votesForMapTwo = (int)stream.ReceiveNext();
