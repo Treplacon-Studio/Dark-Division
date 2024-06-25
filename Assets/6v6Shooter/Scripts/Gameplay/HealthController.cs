cfa9c5745c262119464f04ea436c74f657b03767
austynwhaley <austyn_whaley@yahoo.com>
2024-06-25 09:25:55 -0400

    fixed coliders on player

--- Assets/6v6Shooter/Scripts/Gameplay/HealthController.cs
+++ Assets/6v6Shooter/Scripts/Gameplay/HealthController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,7 +8,6 @@ namespace _6v6Shooter.Scripts.Gameplay
 {
     public class HealthController : MonoBehaviourPunCallbacks
     {
-
         [SerializeField]
         Image healthBar;
 
@@ -16,38 +16,135 @@ namespace _6v6Shooter.Scripts.Gameplay
 
         public bool targetDummy;
 
-        void Start() {
+        private Animator animator;
+        private MovementController movementController;
+        private BoneRotator boneRotator;
+        private List<Rigidbody> ragdollBodies = new List<Rigidbody>();
+
+        void Start()
+        {
             health = startHealth;
             healthBar.fillAmount = health / startHealth;
-        }
 
-        [PunRPC]
-        public void TakeDamage(float damage) {
-            health -= damage;
-            healthBar.fillAmount = health / startHealth;    
-        
-            if (health <= 0f)
-                Die();
-        }
+            animator = GetComponentInChildren<Animator>();
+            movementController = GetComponent<MovementController>();
+            boneRotator = GetComponentInChildren<BoneRotator>();
 
-        void Die() {
-            if (photonView.IsMine && targetDummy is true) {
-                if (targetDummy is true)
-                    photonView.RPC("RegainHealth", RpcTarget.AllBuffered);
-                else
-                    StartCoroutine(Respawn());
+            foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+            {
+                if (rb.gameObject != this.gameObject)
+                {
+                    ragdollBodies.Add(rb);
+                    rb.isKinematic = true;
+                }
             }
         }
 
-        IEnumerator Respawn() {
+        [PunRPC]
+        public void TakeDamage(float damage)
+        {
+            health -= damage;
+            healthBar.fillAmount = health / startHealth;
+
+            if (health <= 0f)
+            {
+                Die();
+            }
+        }
+
+        void Die()
+        {
+            Debug.Log("Die function triggered");
+
+            if (photonView.IsMine)
+            {
+                if (targetDummy)
+                {
+                    photonView.RPC("RegainHealth", RpcTarget.AllBuffered);
+                }
+                else
+                {
+                    Debug.Log("Enabling Ragdoll");
+                    EnableRagdoll();
+                    StartCoroutine(Respawn());
+                }
+            }
+        }
+
+        void EnableRagdoll()
+        {
+            Debug.Log("Ragdoll Enabled");
+            if (animator != null) animator.enabled = false;
+            if (movementController != null) movementController.enabled = false;
+            if (boneRotator != null) boneRotator.enabled = false;
+
+            MonoBehaviour[] scripts = GetComponents<MonoBehaviour>();
+            foreach (var script in scripts)
+            {
+                if (script != this
+                && script != photonView
+                && script != movementController
+                && script != boneRotator)
+                {
+                    script.enabled = false;
+                }
+            }
+
+            foreach (Rigidbody rb in ragdollBodies)
+            {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+                rb.isKinematic = false;
+            }
+
+            Rigidbody mainRb = GetComponent<Rigidbody>();
+            if (mainRb != null)
+            {
+                mainRb.isKinematic = true;
+            }
+        }
+
+        void DisableRagdoll()
+        {
+            if (animator != null) animator.enabled = true;
+            if (movementController != null) movementController.enabled = true;
+            if (boneRotator != null) boneRotator.enabled = true;
+
+            MonoBehaviour[] scripts = GetComponents<MonoBehaviour>();
+            foreach (var script in scripts)
+            {
+                if (script != this
+                && script != photonView
+                && script != movementController
+                && script != boneRotator)
+                {
+                    script.enabled = true;
+                }
+            }
+
+            foreach (Rigidbody rb in ragdollBodies)
+            {
+                rb.isKinematic = true;
+            }
+
+            Rigidbody mainRb = GetComponent<Rigidbody>();
+            if (mainRb != null)
+            {
+                mainRb.isKinematic = false;
+            }
+        }
+
+        IEnumerator Respawn()
+        {
             GameObject respawnText = GameObject.Find("RespawnText");
 
             float respawnTime = 8.0f;
-            while (respawnTime > 0.0f) {
+            while (respawnTime > 0.0f)
+            {
                 yield return new WaitForSeconds(1.0f);
                 respawnTime -= 1.0f;
                 transform.GetComponent<PlayerMotor>().enabled = false;
-                respawnText.GetComponent<Text>().text = "You are killed. Respawning at: " + respawnTime.ToString(".00");
+                respawnText.GetComponent<Text>().text = "You are killed. Respawning at: " + respawnTime.ToString("0.00");
             }
 
             respawnText.GetComponent<Text>().text = "";
@@ -60,7 +157,8 @@ namespace _6v6Shooter.Scripts.Gameplay
         }
 
         [PunRPC]
-        public void RegainHealth() {
+        public void RegainHealth()
+        {
             health = startHealth;
             healthBar.fillAmount = health / startHealth;
         }
