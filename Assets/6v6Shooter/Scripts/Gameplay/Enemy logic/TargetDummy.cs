6d38ecde3bcc43f35d2b4f1219d4d4931ab5a197
Skylar Masson <Smasson1998@gmail.com>
2024-07-07 01:22:57 -0500

    Added moving targets in practice range.

--- Assets/6v6Shooter/Scripts/Gameplay/Enemy logic/TargetDummy.cs
+++ Assets/6v6Shooter/Scripts/Gameplay/Enemy logic/TargetDummy.cs
@@ -5,6 +5,35 @@ using Photon.Pun;
 
 public class TargetDummy :  MonoBehaviourPun, IPunObservable
 {
+
+    public bool isMoveableTarget;
+
+    public float speed = 1f;
+    public float distance = 5f;
+
+    private Vector3 startPosition;
+    private float randomOffset;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        randomOffset = Random.Range(0f, 2f * Mathf.PI);
+    }
+
+    void Update()
+    {
+        if (isMoveableTarget && photonView.IsMine)
+            MoveTarget();
+    }
+
+    private void MoveTarget()
+    {
+        float movement = Mathf.PingPong(Time.time * speed + randomOffset, distance * 2) - distance;
+        Vector3 localPosition = transform.localPosition;
+        localPosition.z = movement;
+        transform.localPosition = localPosition;
+    }
+
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
