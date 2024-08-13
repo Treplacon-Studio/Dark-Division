ff47a54ba795df6fa0a14fb7e40a6da6a17a26bb
Nathan Steendam <bo2nathan@hotmail.com>
2024-08-13 21:21:32 +0200

    [FIX] Script changes and added SO

--- Assets/6v6Shooter/Scripts/Gameplay/Player/Actions/ThrowEquipment.cs
+++ Assets/6v6Shooter/Scripts/Gameplay/Player/Actions/ThrowEquipment.cs
@@ -4,191 +4,139 @@ using UnityEngine;
 
 public class ThrowEquipment : MonoBehaviour
 {
-    [Header("References")]
-    public Transform ThrowPoint;
-    public GameObject ObjectToThrow1;
-    public GameObject ObjectToThrow2;
-    public GameObject StaticObjectToThrow1;
-    public GameObject StaticObjectToThrow2; 
-    public Animator Animator;
-    [SerializeField] public GameObject Weapon;
-    [SerializeField] public Transform HandTransform;
+    public enum ThrowableType { Primary, Secondary }
 
-    [Header("Settings")]
-    public int EquipmentAmmo1;
-    public int EquipmentAmmo2;
-    public float Cooldown;
-    public float ThrowDelay = 0.3f;
+    [Header("References")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject weaponSocket;
+    [SerializeField] private Transform throwPoint;
+    [SerializeField] private float throwCooldown;
 
     [Header("Throwing")]
-    public KeyCode ThrowBtn1 = KeyCode.G;
-    public KeyCode ThrowBtn2 = KeyCode.H;
-    public KeyCode ShortThrowBtn = KeyCode.F;
+    [SerializeField] private KeyCode ThrowLethal = KeyCode.G;
+    [SerializeField] private KeyCode ThrowTactical = KeyCode.H;
+    [SerializeField] private KeyCode ThrowStyleChange = KeyCode.F;
 
-    public float ThrowForce = 16;
-    public float ThrowUpwardForce = 15;
+    [Header("SO references")]
+    [SerializeField] private ThrowableSO lethalThrowableSO;
+    [SerializeField] private ThrowableSO tacticalThrowableSO;
 
-    private bool canThrow;
-    private bool isHolding;
-    private bool isFrozen;
-    private GameObject activeGrenade;
+    private ThrowableSO currentThrowable;
+    private GameObject lethalDummy;
+    private GameObject tacticalDummy;
+    private GameObject currentDummy;
+    private GameObject throwableHulster;
+
+    private bool canThrow = true;
+    private bool isHolding = false;
+    private bool isFrozen = false;
+    private bool isOverhandThrow = true;
 
     private void Start()
     {
-        canThrow = true;
+        //Spawn a dummy for the lethal and tactical
+        lethalDummy = Instantiate(lethalThrowableSO.dummyPrefab, throwableHulster.transform);
+        lethalDummy.SetActive(false);
+        tacticalDummy = Instantiate(tacticalThrowableSO.dummyPrefab, throwableHulster.transform);
+        tacticalDummy.SetActive(false);
     }
 
     private void Update()
-{
-    if (Input.GetKeyDown(ThrowBtn1) && canThrow && EquipmentAmmo1 > 0)
     {
-        // Check if it's a knife or regular throw
-        if (IsKnifeEquipped())
-        {
-            Animator.SetTrigger("pKnifeThrow"); // Set the knife throw trigger
-            Invoke(nameof(FreezeAnimation), 0.1f); // Freeze only for knife throw
-        }
-        else
-        {
-            Animator.SetTrigger("pThrow"); // Set the regular throw trigger
-        }
-
-        isHolding = true;
-        EquipmentAmmo1--;
-
-        if (Weapon != null)
-        {
-            DisableWeaponChildren();
-            EquipGrenade(StaticObjectToThrow1);
-        }
+        PrepareThrow(ThrowLethal, lethalThrowableSO);
+        PrepareThrow(ThrowTactical, tacticalThrowableSO);
+        ChangeThrowStyle();
     }
 
-    if (Input.GetKeyUp(ThrowBtn1) && isHolding)
+    private void PrepareThrow(KeyCode throwKey, ThrowableSO throwable)
     {
-        if (IsKnifeEquipped())
+        if (Input.GetKey(throwKey))
         {
-            UnfreezeAnimation(); // Unfreeze only for knife throw
-            Animator.SetTrigger("pKnifeThrow"); // Finish the knife throw animation
-        }
-        else
-        {
-            Animator.SetTrigger("pFarThrow"); // Transition to the second animation for regular throw
-        }
+            if (canThrow && throwable.HasAmmo())
+            {
+                if (!isHolding)
+                {
+                    isHolding = true;
+                    currentThrowable = throwable;
 
-        Invoke(nameof(ThrowPrimary), ThrowDelay);
-        isHolding = false;
-    }
+                    // Select the appropriate dummy based on the throwable type
+                    currentDummy = throwable == lethalThrowableSO ? lethalDummy : tacticalDummy;
 
-    if (Input.GetKeyDown(ThrowBtn2) && canThrow && EquipmentAmmo2 > 0)
-    {
-        Animator.SetTrigger("pThrow"); // Regular throw, no freezing needed
-        isHolding = true;
-        EquipmentAmmo2--;
+                    // Move the dummy to the weaponSocket
+                    currentDummy.transform.SetParent(weaponSocket.transform);
+                    currentDummy.transform.localPosition = Vector3.zero;
+                    currentDummy.transform.localRotation = Quaternion.identity;
+                    currentDummy.SetActive(true);
 
-        if (Weapon != null)
-        {
-            DisableWeaponChildren();
-            EquipGrenade(StaticObjectToThrow2);
+                    // Set the appropriate animation trigger
+                    if (throwable.isThrowingKnife)
+                    {
+                        animator.SetTrigger("pPrepThrowKnife");
+                    }
+                    else
+                    {
+                        animator.SetTrigger("pPrepThrow");
+                    }
+                }
+
+                if (Input.GetKeyUp(throwKey))
+                {
+                    ThrowAnimation(currentThrowable);
+                    isFrozen = false;
+                    isHolding = false;
+
+                    currentDummy.SetActive(false);
+                    currentDummy.transform.position = throwableHulster.transform.position;
+                    currentDummy = null;
+                }
+            }
         }
     }
 
-    if (Input.GetKeyUp(ThrowBtn2) && isHolding)
-    {
-        Animator.SetTrigger("pFarThrow"); // Transition to the second animation for regular throw
-        Invoke(nameof(ThrowSecondary), ThrowDelay);
-        isHolding = false;
-    }
-}
 
-    private void ThrowPrimary()
+    private void ChangeThrowStyle()
     {
-        PerformThrow(ObjectToThrow1);
-    }
-
-    private void ThrowSecondary()
-    {
-        PerformThrow(ObjectToThrow2);
-    }
-
-    private void PerformThrow(GameObject throwable)
-    {
-        Throw(throwable);
-        
-        if (activeGrenade != null)
+        if (Input.GetKeyDown(ThrowStyleChange) && isHolding)
         {
-            activeGrenade = null;
+            isOverhandThrow = !isOverhandThrow;
+        }
+    }
+
+    private void ThrowAnimation(ThrowableSO throwable)
+    {
+        {
+            //Adjust trigger values Austyn
+            string triggerName = throwable.isThrowingKnife
+            ? (isOverhandThrow ? "pThrowKnifeOverhand" : "pThrowKnifeUnderhand")
+            : (isOverhandThrow ? "pThrowOverhand" : "pThrowUnderhand");
+            animator.SetTrigger(triggerName);
         }
 
-        Invoke(nameof(ReEnableWeaponChildren), 0.5f);
-    }
-
-    private void Throw(GameObject objectToThrow)
-    {
         canThrow = false;
-        GameObject projectile = Instantiate(objectToThrow, ThrowPoint.position, ThrowPoint.rotation);
-        Rigidbody projectileRB = projectile.GetComponent<Rigidbody>();
-
-        Vector3 forceDirection = ThrowPoint.transform.forward;
-        Vector3 addForce = forceDirection * ThrowForce + ThrowPoint.transform.up * ThrowUpwardForce;
-        projectileRB.AddForce(addForce, ForceMode.Impulse);
-
-        if (activeGrenade != null)
-        {
-            Destroy(activeGrenade);
-        }
-
-        Invoke(nameof(ResetThrow), Cooldown);
     }
 
-    private void ResetThrow()
+    public void Throw()
     {
+        //make sure to use object pooling
+        GameObject throwableInstance = Instantiate(currentThrowable.actualPrefab, throwPoint.position, throwPoint.rotation);
+
+        if (isOverhandThrow == true)
+        {
+            //Apply the old throwing (upward and forward force)
+        }
+        else
+        {
+            //Apply the old throwing (upward and forward force)
+        }
+
+        StartCoroutine(ThrowCooldown(currentThrowable.coolDownTime));
+    }
+
+    private IEnumerator ThrowCooldown(float coolDownTime)
+    {
+        currentThrowable = null;
+        
+        yield return new WaitForSeconds(coolDownTime);
         canThrow = true;
-    }
-
-    private void DisableWeaponChildren()
-    {
-        foreach (Transform child in Weapon.transform)
-        {
-            child.gameObject.SetActive(false);
-        }
-    }
-
-    private void ReEnableWeaponChildren()
-    {
-        foreach (Transform child in Weapon.transform)
-        {
-            child.gameObject.SetActive(true);
-        }
-    }
-
-    private void EquipGrenade(GameObject grenadePrefab)
-    {
-        if (grenadePrefab != null)
-        {
-            activeGrenade = Instantiate(grenadePrefab, HandTransform);
-        }
-    }
-
-    public void FreezeAnimation()
-{
-    if (isHolding && !isFrozen && IsKnifeEquipped())
-    {
-        Animator.speed = 0; // Pause the animation only for knife throw
-        isFrozen = true;
-    }
-}
-
-public void UnfreezeAnimation()
-{
-    if (IsKnifeEquipped())
-    {
-        Animator.speed = 1; // Resume the animation only for knife throw
-        isFrozen = false;
-    }
-}
-
-    private bool IsKnifeEquipped()
-    {
-        return ObjectToThrow1 != null && ObjectToThrow1.name == "ThrowKnifePrefab";
     }
 }
